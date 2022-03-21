d4fc0ee6933f1836d116b89010c6cfe1308427d8
Josef Leinwber <josef.leinweber@code.berlin>
2022-03-21 10:31:06 +0100

    tictacto cs50 task, not finished

---
+++ cs50/search/tictactoe/tictactoe.py
@@ -0,0 +1,262 @@
+"""
+Tic Tac Toe Player
+"""
+
+from copy import deepcopy
+import math
+import numpy as np
+from msilib.schema import Error
+
+
+X = "X"
+O = "O"
+EMPTY = None
+INFINITY = 100000000
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    x_count = 0
+    o_count = 0
+
+    # For each O or X entry in the board add one to the o or x counter
+    for row in board: 
+        for field in row:
+            if field == X:
+                x_count += 1
+
+            if field == O:
+                o_count += 1
+
+    if x_count == o_count:
+        return X
+
+    else:
+        return X
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    possible_actions = set()
+
+    for row in range(len(board)):
+        for column in range(len(board[0])):
+
+            if board[row][column] == None:
+                possible_actions.add((row, column))
+
+    return possible_actions
+
+
+def validate_action(board, action):
+
+    if board[action[0]][action[1]] == EMPTY:
+        return True
+
+    else:
+        return False
+    
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    # Get the current player
+    current_player = player(board)
+
+    if not validate_action(board, action):
+        print("---------------------")
+        print("not a valid action")
+        print("Action : ", action)
+        print("Board : \n", board)
+        raise ValueError
+
+    deep_copy_of_board = deepcopy(board)
+    
+    deep_copy_of_board[action[0]][action[1]] = current_player
+
+    print("\nafter taking a trun\n")
+    print("Action : ", action)
+    print(" inital Board : \n", board)
+    print("result board: \n", deep_copy_of_board)
+
+
+    return deep_copy_of_board
+
+def evaluate_diagonals(board):
+    if board[0][0] == board[1][1] == board[2][2]:
+        return (0, 0)
+
+    if board[2][0] == board[1][1] == board[0][2]:
+        return (2, 0)
+
+    return None
+
+def get_winner(board, coordinates):
+    winner = board[coordinates[0]][coordinates[1]]
+    return winner
+
+def same_field_values(array):
+    return all(x == array[0] for x in array)
+
+def evaluating_horizontals(board):
+    for row_index in range(len(board)):
+        row_values = []
+        for column_index in range(len(board)):
+            row_values.append(board[row_index][column_index])
+
+        if row_values[0] != EMPTY and same_field_values(row_values):
+            return (row_index, column_index)
+
+    return None
+
+
+def get_winner_coordinates(board):
+    coordinates = ()
+
+    # Evaluating horizontals
+    if evaluating_horizontals != None:
+        coordinates = evaluating_horizontals(board)
+
+        
+
+
+    # Evaluating verticals
+
+    # Evaluating diagonals
+
+
+    return coordinates
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    winner = None
+    
+    
+    
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+
+    game_is_over = False
+
+    # If there is a winner the game is over
+    if winner(board) != None:
+
+        game_is_over = True
+        return game_is_over
+
+    # Else if there is no winner check if there are no more empty fields
+    # in the board
+    else:
+
+        empty_fields_in_board = any(None in row for row in board)
+
+        if not empty_fields_in_board:
+
+            game_is_over = True
+            return game_is_over
+
+        else:
+
+            return game_is_over
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    if terminal(board):
+        winner_of_game = winner(board)
+
+        if winner_of_game == X:
+            return 1
+
+        elif winner_of_game == O:
+            return -1
+        
+        elif winner_of_game == None:
+            return 0
+
+        else:
+            raise ValueError
+
+
+
+
+
+def min_value(board):
+    v = INFINITY
+
+    # If the game is over return the utility (1, 0 -1)
+    if terminal(board):
+        return utility(board)
+
+    # Get all the possible actions
+    possible_actions = actions(board)
+
+    for action in possible_actions:
+        resulting_board_for_action = result(board, action)
+        v = min(v, max_value(resulting_board_for_action))
+    return v
+
+def max_value(board):
+    v = -INFINITY
+
+    # If the game is over return the utility (1, 0 -1)
+    if terminal(board):
+        return utility(board)
+
+    # Get all the possible actions
+    possible_actions = actions(board)
+
+    for action in possible_actions:
+        resulting_board_for_action = result(board, action)
+        v = max(v, min_value(resulting_board_for_action))
+    
+    return v
+
+
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    # Check if the game is over
+    if terminal(board):
+        return None
+
+    values_of_action = []
+
+    for action in actions(board):
+
+        value = max_value(result(board, action))
+
+        values_of_action.append((value, action))
+
+    values_of_action.sort()
+
+    return values_of_action[-1][1]
+
+
+
+    
